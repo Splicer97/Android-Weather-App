a52d3589596adcf9bf2f789165d3357991d86f0b
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-19 21:02:41 +0300

    lesson_1

---
+++ app/src/main/java/com/splicer/androidweatherapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.splicer.androidweatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.splicer.androidweatherapp.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+const val API_KEY = "b0676713ebfe49efbf8174320221907"
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bGet.setOnClickListener {
+            getResult("London")
+        }
+    }
+
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json?key= $API_KEY&q=$name&aqi=no"
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            { responce ->
+                val obj = JSONObject(responce)
+                val temp = obj.getJSONObject("current")
+                Log.d("MyLog", "Responce $responce") },
+            { Log.d("MyLog", "Volley Error $it") })
+        queue.add(stringRequest)
+    }
+}
