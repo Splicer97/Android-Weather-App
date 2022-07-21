9da1afe21992ea8c953d62a792089944ee163bae
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-21 23:12:28 +0300

    lesson_8

--- app/src/main/java/com/splicer/androidweatherapp/MainViewModel.kt
+++ app/src/main/java/com/splicer/androidweatherapp/MainViewModel.kt
@@ -2,8 +2,10 @@ package com.splicer.androidweatherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.splicer.androidweatherapp.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
+
