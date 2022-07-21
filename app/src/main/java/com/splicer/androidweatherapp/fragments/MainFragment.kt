646a1fca9f216aaa68faff2faad54f30b03c52fc
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-21 22:54:54 +0300

    lesson_7

--- app/src/main/java/com/splicer/androidweatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/splicer/androidweatherapp/fragments/MainFragment.kt
@@ -9,11 +9,16 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.splicer.androidweatherapp.R
+import com.splicer.androidweatherapp.adapters.VpAdapter
 import com.splicer.androidweatherapp.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private val fList = listOf(HoursFragment.newInstance(), DaysFragment.newInstance())
+    private val tList = listOf("Hours", "Days")
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
@@ -26,21 +31,29 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        checkPermission()
     }
+
+    private fun init() = with(binding) {
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp) { tab, pos ->
+            tab.text = tList[pos]
+        }.attach()
+    }
+
 
     private fun permissionListener() {
         pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-
-
         }
     }
 
-private fun checkPermission() {
-    if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
-        permissionListener()
-        pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
-}
 
     companion object {
         @JvmStatic
