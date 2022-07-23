997fed729a3a7e0811bf4a748bd99b1b6b9a4058
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-23 20:44:22 +0300

    lesson_10

--- app/src/main/java/com/splicer/androidweatherapp/fragments/HoursFragment.kt
+++ app/src/main/java/com/splicer/androidweatherapp/fragments/HoursFragment.kt
@@ -5,23 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.splicer.androidweatherapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.splicer.androidweatherapp.adapters.WeatherAdapter
+import com.splicer.androidweatherapp.adapters.WeatherModel
+import com.splicer.androidweatherapp.databinding.FragmentHoursBinding
 
 
 class HoursFragment : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    ): View {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "","12:00",
+                "Sunny","25ºC",
+                "", "", "",""),
+            WeatherModel(
+                "","13:00",
+                "Sunny","25ºC",
+                "", "", "",""),
+            WeatherModel(
+                "","14:00",
+                "Sunny","35ºC",
+                "", "", "","")
+        )
+        adapter.submitList(list)
     }
 
     companion object {
-
         @JvmStatic
-        fun newInstance() =
-            HoursFragment()
+        fun newInstance() = HoursFragment()
     }
 }
