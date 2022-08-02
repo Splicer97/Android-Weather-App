704314bb9d3b24298628dd7dada1299feb1b6794
Nikita_Kovantsov <kovancov@gmail.com>
2022-08-02 09:54:12 +0300

    lesson_13

--- app/src/main/java/com/splicer/androidweatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/splicer/androidweatherapp/fragments/MainFragment.kt
@@ -64,24 +64,23 @@ class MainFragment : Fragment() {
         }
     }
 
-    private fun requestWeatherData(city: String) {
-        val url = "https://api.weatherapi.com/v1/current.json?key=" +
+    private fun requestWeatherData(city: String){
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
                 API_KEY +
                 "&q=" +
                 city +
                 "&days=" +
                 "3" +
-                "&aqi=no"
-
+                "&aqi=no&alerts=no"
         val queue = Volley.newRequestQueue(context)
         val request = StringRequest(
             Request.Method.GET,
             url,
-            { result ->
-                Log.d("MyLog", "Result: $result")
+            {
+                    result -> parseWeatherData(result)
             },
-            { error ->
-                Log.d("MyLog", "Error: $error")
+            {
+                    error -> Log.d("MyLog", "Error: $error")
             }
         )
         queue.add(request)
@@ -89,18 +88,50 @@ class MainFragment : Fragment() {
 
     private fun parseWeatherData(result: String) {
         val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel){
         val item = WeatherModel(
             mainObject.getJSONObject("location").getString("name"),
             mainObject.getJSONObject("current").getString("last_updated"),
             mainObject.getJSONObject("current")
                 .getJSONObject("condition").getString("text"),
             mainObject.getJSONObject("current").getString("temp_c"),
-            "",
-            "",
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
             mainObject.getJSONObject("current")
                 .getJSONObject("condition").getString("icon"),
-            ""
+            weatherItem.hours
         )
+        Log.d("MyLog", "City: ${item.maxTemp}")
+        Log.d("MyLog", "Time: ${item.minTemp}")
+        Log.d("MyLog", "Time: ${item.hours}")
+    }
+
+    private fun parseDays(mainObject: JSONObject): List<WeatherModel>{
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name =  mainObject.getJSONObject("location").getString("name")
+        for (i in 0 until daysArray.length()){
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("text"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition")
+                    .getString("icon"),
+                day.getJSONArray("hour").toString()
+            )
+            list.add(item)
+        }
+        return list
     }
 
     companion object {
